0b31db6a01fb130504d00ea5b2c45543a1c05e95
sweta <sg060479@gmail.com>
2025-09-08 14:19:29 -0400

    Day 2

---
+++ DAY_2/namespace.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std ;
+
+// std - standard c++ namespace 
+int main(){
+    std::cout<<"namespace";
+    return 0;
+}
---
+++ DAY_2/output.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std ;
+
+
+int main(){
+    cout<<" heelo hi there";
+    return 0;
+}
